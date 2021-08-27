e8dbb6e7717192dfc307f55dfd8e221830712e3a
Tatiana S <tatyana-selyuk@yandex.com>
2021-08-27 17:55:24 +0300

    Added def. user;

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -3,8 +3,8 @@ import {createContext} from "react";
 export const CurrentUserContext = createContext();
 
 export const defaultUser = {
-    _id: '',
+    _id: '6127f4b919370e0013efec09',
+    name: 'Загрузка...',
     about: 'Загрузка...',
-    avatar: 'https://64.media.tumblr.com/1d6abee87aa71fc219c2312ba0c7c7c5/tumblr_p69s21P7KM1w3y4ilo1_500.gifv',
-    name: 'Загрузка...'
+    avatar: 'https://64.media.tumblr.com/1d6abee87aa71fc219c2312ba0c7c7c5/tumblr_p69s21P7KM1w3y4ilo1_500.gifv'
 };
